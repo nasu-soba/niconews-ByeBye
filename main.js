001f6780f650469791c377dc859c1217aaf15f03
nasu2091 <nevero5723@icloud.com>
2021-04-27 11:33:43 +0900

    Revert "Merge remote-tracking branch 'origin/main' into main"
    This reverts commit ca8d729784f6483ea175624a8ed416f4980c5d08.

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-'use strict';
 window.onload = function(){
-    let div = document.querySelectorAll('img')
-    for (let n in div) {
-        print(div[n])
-        // div[n].src = "hide-eye.png";
-        div[n].src = chrome.extension.getURL("hide-eye.png")
-        div[n].style.visibility = "hidden";
+    let div = document.getElementsByClassName('Grid Marquee')
+    if(div){
+        div[0].style.display ="none"
     }
 }
